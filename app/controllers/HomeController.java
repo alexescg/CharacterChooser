064120b51bef1bfbc0ecf448ef0fb392e879c7f2
alex <aeg0204@gmail.com>
2017-12-22 09:21:07 -0700

    Initial commit

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
+import play.mvc.*;
+
+import java.util.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+        Map<String, String> playerMap = new HashMap<>();
+
+        List<String> players = new ArrayList<>();
+        players.add("Luis");
+        players.add("Alito");
+        players.add("Mauricio");
+        players.add("Huguito");
+        players.add("Omar");
+        players.add("Marco");
+
+        List<String> heroes = new ArrayList<>();
+        heroes.add("Verde");
+        heroes.add("Morado");
+        heroes.add("Rojo");
+        heroes.add("Azul");
+        heroes.add("Gris");
+        heroes.add("Amarillo");
+        for (String player : players) {
+            heroes.remove(assignHeroes(player, heroes, playerMap));
+        }
+        JsonNode content = Json.toJson(playerMap);
+        return ok(content);
+    }
+
+    private String assignHeroes(String player, List<String> heroes, Map<String, String> playerMap) {
+        Random random = new Random();
+        String hero = heroes.get(random.nextInt(heroes.size()));
+        playerMap.put(player, hero);
+        return hero;
+    }
+}
---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := """hero_chooser"""
+organization := "alexescg.com.github"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.12.4"
+
+libraryDependencies += guice
